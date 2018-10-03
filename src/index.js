42c7d3e98865ef18a9ff68d1b68ecaf0802f0bf6
aliaksandraRybak <aliaksandra.rybak@gmail.com>
2018-10-03 11:56:36 +0300

    smart calculator

--- src/index.js
+++ src/index.js
@@ -1,27 +1,131 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+      this.value = initialValue;
+      this.expression = [initialValue];
+
+      this.priorities = new Map();
+      this.priorities.set('+', 1);
+      this.priorities.set('-', 1);
+      this.priorities.set('*', 2);
+      this.priorities.set('/', 2);
+      this.priorities.set('**', 3);
   }
 
   add(number) {
-    // your implementation
+    this.expression.push('+');
+    this.expression.push(number);
+
+    this.value = this.createStack(this.expression);
+
+    return this;
   }
-  
+    
   subtract(number) {
-    // your implementation
+    this.expression.push('-');
+    this.expression.push(number);
+
+    this.value = this.createStack(this.expression);
+
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.expression.push('*');
+    this.expression.push(number);
+
+    this.value = this.createStack(this.expression);
+
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.expression.push('/');
+    this.expression.push(number);
+
+    this.value = this.createStack(this.expression);
+
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.expression.push('**');
+    this.expression.push(number);
+
+      this.value = this.createStack(this.expression);
+
+      return this;
+  }
+
+  createStack(expression) {
+    this.res = [];
+    this.operations = [];
+      
+    expression.forEach((elem) => {
+      if (this.isANumber(elem)) {
+        this.res.push(elem);
+      } else {
+        let last = this.operations[this.operations.length - 1];
+
+        while(this.priorities.get(last) >= this.priorities.get(elem)) {
+          if (this.priorities.get(elem) == Math.max(...this.priorities.values())) break;
+
+          this.res.push(this.operations.pop());
+          last = this.operations[this.operations.length - 1];
+        } 
+
+        this.operations.push(elem);
+      }
+    });
+
+    while (this.operations.length != 0) {
+      this.res.push(this.operations.pop());
+    }
+
+    this.calculate(this.res);
+    return this.res;
+  }
+
+  calculate (stack) {
+    for (let i = 0; i < stack.length; i++) {
+      let output = null;
+
+      switch (stack[i]) {
+        case '+':
+          output = stack[i - 2] + stack[i - 1];
+          stack.splice(i - 2, 3, output);
+          i -= 2;
+          break;
+        case '-':
+          output = stack[i - 2] - stack[i - 1];
+          stack.splice(i - 2, 3, output);
+          i -= 2;
+          break;
+        case '*':
+          output = stack[i - 2] * stack[i - 1];
+          stack.splice(i - 2, 3, output);
+          i -= 2;
+          break;
+        case '/':
+          output = stack[i - 2] / stack[i - 1];
+          stack.splice(i - 2, 3, output);
+          i -= 2;
+          break;
+        case '**':
+          output = stack[i - 2] ** stack[i - 1];
+          stack.splice(i - 2, 3, output);
+          i -= 2;
+          break;
+      } 
+    }
+  }
+
+  isANumber(x) {
+  return !isNaN(parseFloat(x)) && isFinite(x);
+  }
+
+  valueOf() {
+    this.value = this.createStack(this.expression);
+      return this.value[0];
   }
 }
-
 module.exports = SmartCalculator;
